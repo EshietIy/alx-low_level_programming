3f7bfe6c4b6ab0f8a4b42147aef4503bc0813be3
EshietIy <eshietinyang@gmail.com>
2021-06-17 10:27:15 -0700

    use printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-char str = "with proper grammar, but the outcome is a piece of art, ";
-printf("%s",%str);
+printf("%c","with proper grammar, but the outcome is a piece of art, ");
 return (0);
 }
