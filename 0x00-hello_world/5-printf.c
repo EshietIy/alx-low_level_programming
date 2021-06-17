cb765eb8fc812b70159b158f77ad4f75f7398a2b
EshietIy <eshietinyang@gmail.com>
2021-06-17 10:07:40 -0700

    use printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - primary function serves as starting point of the program
+ *Return: 0 upon the reaching the end of the program
+ */
+int main(void)
+{
+prinf("with proper grammar, but the outcome is a piece of art,")
+return (0);
+}
