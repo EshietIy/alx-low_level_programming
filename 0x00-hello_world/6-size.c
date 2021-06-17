daf736770ed36081d4199cd418984e1105fb1d3d
EshietIy <eshietinyang@gmail.com>
2021-06-17 13:11:46 -0700

    print differnt types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <limits.h>
 /**
  *main - primary function serves as starting point of the program
  *Return: 0 upon the reaching the end of the program
  */
 int main(void)
 {
-printf("Size of char : %d .\n", (sizeof(char));
-printf("Size of int : %d .\n", (sizeof(int));
-printf("Size of long : %d .\n", (sizeof(long));
-printf("Size of long long : %d. \n", (sizeof(long long));
-printf("Size of float : %d .\n", (sizeof(float));
+printf("Size of a char : %d \n", (sizeof(char));
+printf("Size of an int : %d \n", (sizeof(int));
+printf("Size of a long : %d \n", (sizeof(long));
+printf("Size of a long long : %d \n", (sizeof(long long));
+printf("Size of a float : %d \n", (sizeof(float));
 return (0);
 }
