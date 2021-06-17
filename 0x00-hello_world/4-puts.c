db5b2be35140c6043141621295d0a6aa17681697
EshietIy <eshietinyang@gmail.com>
2021-06-17 09:47:05 -0700

    C program that prints with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ *main - primary function serves as starting point of the program
+ *Return: 0 upon the reaching the end of the program
+ */
 int main(void)
 {
 char str[] = "\"Programming is like building a multilingual puzzle";
