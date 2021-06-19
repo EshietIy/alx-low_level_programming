7077a02cc9c1553d77d4747f8d8753d6deecce01
EshietIy <eshietinyang@gmail.com>
2021-06-19 13:09:04 -0700

    fixed code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@ int x;
 long y;
 long long int z;
 float f;
-printf("Size of a char : %ld byte(s)\n", sizeof(ch));
-printf("Size of an int : %ld byte(s) \n", sizeof(x));
-printf("Size of a long : %ld byte(s)\n", sizeof(y));
-printf("Size of a long long int : %ld byte(s)\n", sizeof(z));
-printf("Size of a float : %ld byte(s)\n", sizeof(f));
+printf("Size of a char: %ld byte(s)\n", sizeof(ch));
+printf("Size of an int: %ld byte(s) \n", sizeof(x));
+printf("Size of a long int: %ld byte(s)\n", sizeof(y));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(z));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
