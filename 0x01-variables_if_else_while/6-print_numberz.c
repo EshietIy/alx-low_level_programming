3df87e36a8eb96a4e80134289b86032dfa92ecbd
EshietIy <eshietinyang@gmail.com>
2021-06-20 07:45:58 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - function main prints lowercase in new line
+*Return: 0
+*/
+int main(void)
+{
+int ch;
+char nl;
+nl = '\n';
+for (ch = '0'; ch <= '9'; ch++)
+{
+putchar(ch);
+}
+putchar(nl);
+return (0);
+}
