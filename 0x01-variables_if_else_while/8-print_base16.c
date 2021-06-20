fa441cca4cf393e14a13a8020085a8b48fa07ec3
EshietIy <eshietinyang@gmail.com>
2021-06-20 13:46:19 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+*main - function main prints lowercase in new line
+*Return: 0
+*/
+int main(void)
+{
+char ch;
+for (ch = '0'; ch >= '9'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'a'; ch >= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
