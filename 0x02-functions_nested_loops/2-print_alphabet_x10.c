a54c545c6f78f4aa7e7b1a6a6b88ea527489bdd8
EshietIy <eshietinyang@gmail.com>
2021-06-23 07:10:11 -0700

    print all alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - print a-z
+ *
+ *Return: null value
+ */
+
+void print_alphabet_x10(void)
+{
+char alph;
+int x;
+alph = 'a';
+for (x = 0; x <= 9; x++)
+{
+while (alph <= 'z')
+{
+_putchar(aplh);
+alph++;
+}
+_putchar('/n')
+}
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,3 @@
 extern int _putchar(char c);
 extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
