1b47e066c1f9a48bbb5f9a89935624cb74fa4548
EshietIy <eshietinyang@gmail.com>
2021-06-23 03:11:58 -0700

    print using _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,18 @@
-#include<stdio.h>
-/*
+#include "holberton.h"
+/**
  *main - start of program
  *Return: 0
  */
 int main(void)
 {
-printf("Holberton\n");
+char i[] = "Holberton";
+int x;
+x = 0;
+while (x <= 8)
+{
+_putchar(i[x]);
+x++;
+}
+_putchar('\n');
 return (0);
 }
