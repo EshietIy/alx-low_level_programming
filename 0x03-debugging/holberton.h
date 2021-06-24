8a62dc38909e9eb912147fdcec5218eae486e96c
EshietIy <eshietinyang@gmail.com>
2021-06-24 12:43:27 -0700

    This program converts a date to the day of year and determines how many days are left in the year

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,10 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-
 #include <stdio.h>
-
-void positive_or_negative(int i);
-
-int largest_number(int a, int b, int c);
-
+extern void positive_or_negative(int i);
+extern int largest_number(int a, int b, int c);
+extern void print_remaining_days(int month, int day, int year);
 #endif /* HOLBERTON_H */
