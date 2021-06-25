6682a65376d75aebe1a1bffc3e90ce14cd14e059
EshietIy <eshietinyang@gmail.com>
2021-06-25 14:12:39 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,7 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
 #include <stdio.h>
-extern void positive_or_negative(int i);
-extern int largest_number(int a, int b, int c);
-extern void print_remaining_days(int month, int day, int year);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+
 #endif /* HOLBERTON_H */
