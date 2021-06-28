10f5a46a02d523e074de2de1e7d0048b1b8f071d
EshietIy <eshietinyang@gmail.com>
2021-06-28 09:28:55 -0700

    a function that prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,18 @@
  */
 void puts_half(char *str)
 {
-int l = strlen(str);
+int i;
+int l;
+l = strlen(str);
 if (l % 2 == 0)
 {
-for (int i = (l / 2); i <= l; i++)
+for (i = (l / 2); i <= l; i++)
 putchar(str[i]);
 putchar('\n');
 }
 else
 {
-for (int i = (l / 2) + 1; i <= l; i++)
+for (i = (l / 2) + 1; i <= l; i++)
 putchar(str[i]);
 putchar('\n');
 }
