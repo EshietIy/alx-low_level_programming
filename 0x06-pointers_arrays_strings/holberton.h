79db91b8de868f92629ab2652229022177c05f1e
EshietIy <eshietinyang@gmail.com>
2021-06-30 22:03:27 -0700

    a function that prints a buffer.

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,5 +1,6 @@
-har *infinite_add(char *n1, char *n2, char *r, int size_r);
-oid print_number(int n);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
 char *rot13(char *str);
 int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
