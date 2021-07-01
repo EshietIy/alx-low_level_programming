54ca410c5973699cc2e55406813f224e59d782b4
EshietIy <eshietinyang@gmail.com>
2021-06-30 21:40:28 -0700

    a function that compares two strings.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,53 +1,29 @@
 #include "holberton.h"
 
 /**
- * _strlen - Returns length of a string
- * @s: Pointer to string
- * Return: Length of c
-*/
+ * _strcmp - compare strings
+ * @s1: first input char
+ * @s2: second input char
+ *
+ * Description: the function compares two strings
+ * Return: character count
+ */
 
-int _strlen(char *s)
-{
-int index;
-index = 0;
-while (s[index] != '\0')
-{
-index++;
-}
-return (index);
-}
-
-/**
- * _strcmp - Compares two strings
- * @s1: Pointer to first string
- * @s2: Pointer to second string
- * Return: Return -15 if s1 < s2, 15 if s1 > s2, else 0
-*/
 int _strcmp(char *s1, char *s2)
 {
-int slen1;
-int slen2;
-int i;
-int value;
-slen1 = _strlen(s1);
-slen2 = _strlen(s2);
-value = 0;
-if (slen1 < slen2)
-value = -15;
-if (slen1 > slen2)
-value = 15;
-for (i = 0; i < slen1; i++)
+int i, diff;
+for (i = 0; s1[i] != '\0'; ++i)
 {
-if (s1[i] < s2[i])
-{
-value = -15;
-break;
-}
 if (s1[i] > s2[i])
 {
-value =  15;
-break;
+diff = s1[i] - s2[i];
+return (diff);
+}
+else if (s1[i] < s2[i])
+{
+diff = s1[i] - s2[i];
+return (diff);
 }
 }
-return (value);
+return (0);
 }
