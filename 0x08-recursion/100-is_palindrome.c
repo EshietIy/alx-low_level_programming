e73bc7ae27881d809b21610d70aed2246492011f
EshietIy <eshietinyang@gmail.com>
2021-07-07 14:26:27 -0700

    fixed code

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,19 @@
 #include "holberton.h"
+#include"holberton.h"
+/**
+ *_strlen_recursion - function that return the length of string
+ *@s: argument to the function
+ *Return: 1 or 0
+ */
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+return (1 + _strlen_recursion(s + 1));
+}
+else
+return (0);
+}
 
 /**
  * checker - checks if palindrome or not
