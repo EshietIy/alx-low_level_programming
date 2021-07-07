10b09d8c71463615da20c3167ad6f99d3dfc3ac3
EshietIy <eshietinyang@gmail.com>
2021-07-07 13:57:49 -0700

    a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * wildcmp - compares two strings and returns 1 if the strings can be
+ * considered identical, otherwise return 0
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 or 0 if they are identical or not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,3 +1,4 @@
+int wildcmp(char *s1, char *s2);
 int checker(char *s, int l, int i);
 int is_prime_number(int n);
 int _sqrt_recursion(int n);
