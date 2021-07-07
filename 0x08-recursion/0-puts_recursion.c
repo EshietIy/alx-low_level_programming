5e0ef3a61677fbce195621e28dde816abe9b85f8
EshietIy <eshietinyang@gmail.com>
2021-07-07 11:57:40 -0700

    a function that prints a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-if (*(s + 1) == '\0')
+if (*s != 0)
 {
 _putchar(*s);
-return;
-}
-_putchar(*s);
-_puts_recursion((s + 1));
+_puts_recursion(++s);
+} else
 _putchar('\n');
 }
