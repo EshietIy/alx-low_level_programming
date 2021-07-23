c8e21748a563c35a7b4cf98d7ac53464cbb0f9a0
EshietIy <eshietinyang@gmail.com>
2021-07-22 17:02:57 -0700

    a function that prints anything.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * struct prints - Defines a printer to data types
+ * @notation: type of data type
+ * @print_dType: pointer to function that prints data types
+ * according to their notation
+ */
+
+typedef struct prints
+{
+char *notation; /** format specifiers **/
+void (*print_dType)(va_list argptr);
+} prints_t;
+
+void print_char(va_list argptr);
+void print_int(va_list argptr);
+void print_float(va_list argptr);
+void print_string(va_list argptr);
 #endif
