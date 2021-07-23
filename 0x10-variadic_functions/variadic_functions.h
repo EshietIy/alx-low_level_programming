47ea47e405fa78b2ca81314dd78036d501ddbf76
EshietIy <eshietinyang@gmail.com>
2021-07-22 17:09:29 -0700

    a function that prints anything.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,7 @@
 void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #include <stdio.h>
 #include <stdarg.h>
 /**
