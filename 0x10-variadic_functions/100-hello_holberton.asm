62217f95142545d5d285774b9bef157286b70a33
EshietIy <eshietinyang@gmail.com>
2021-07-22 17:12:29 -0700

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+
+section .text
+	global	main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, 17
+	syscall
+
+section .rodata
+	msg: db "Hello, Holberton", 0xa
+len: equ $ - msg
