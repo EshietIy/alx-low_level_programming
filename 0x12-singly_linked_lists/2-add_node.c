6308bc251a7faf713bb2abad2e24766b04a50b32
EshietIy <eshietinyang@gmail.com>
2021-07-29 13:10:59 -0700

    a function that adds a new node at the beginning of a list_t

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+ * _strlen - return length of string
+ *
+ * @s: string to count
+ *
+ * Return: the size
+ */
+int _strlen(char *s)
+{
+int counter = 0;
+while (s[counter] != 0)
+{
+counter++;
+}
+return (counter);
+}
+
+/**
  *add_node - a function that adds to a noode
  *@head:  pointer of head to be added
  *@str: string to be added to each node
