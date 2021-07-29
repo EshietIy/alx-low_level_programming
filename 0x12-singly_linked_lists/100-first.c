35c953076afe3d880485661a9724850bee213ebc
EshietIy <eshietinyang@gmail.com>
2021-07-29 14:01:10 -0700

    awake function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * awake - function runs before main
+ *Return: void
+ */
+
+void awake(void)
+{
+printf(
+"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+);
+}
