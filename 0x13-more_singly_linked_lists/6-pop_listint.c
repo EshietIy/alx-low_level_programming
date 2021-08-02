2d4bec4e3a9a01ab6a5c81aae1c68ed3e5ef35bd
EshietIy <eshietinyang@gmail.com>
2021-08-02 13:58:32 -0700

    a function thats pops the 1st element and return its data

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *pop_listint - a function that pops the first element of list
+ *@head: parameter head pointer
+ *Return: int data of popped element
+ */
+int pop_listint(listint_t **head)
+{
+int n;
+listint_t *current;
+current = *head;
+if (current == NULL)
+return (0);
+if(current)
+{
+n = current->n;
+*head = current->next;
+free(current);
+}
+return (n);
+}
