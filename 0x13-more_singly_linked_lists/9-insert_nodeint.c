9e9b136d13a1d03852ef8789fec7839fe1d0c6f7
EshietIy <eshietinyang@gmail.com>
2021-08-03 03:43:18 -0700

    add node base on index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,19 +9,32 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *current = *head, *tmp, *newNode = malloc(sizeof(listint_t));
-unsigned int i;
-if (!newNode)
-return (NULL);
-for (i = 0; i < idx - 1; i++)
+unsigned int i = 0;
+listint_t *current_node = *head, *new_node_ptr;
+if (idx == 0)
 {
-if (!current)
+new_node_ptr = malloc(sizeof(listint_t));
+if (!new_node_ptr)
 return (NULL);
-current = current->next;
+new_node_ptr->n = n;
+new_node_ptr->next = *head;
+*head = new_node_ptr;
+return (new_node_ptr);
 }
-newNode->n = n;
-tmp = current->next;
-current->next = newNode;
-newNode->next = tmp;
-return (newNode);
+while (current_node && ((i + 1) != idx))
+{
+current_node = current_node->next;
+i++;
+}
+if ((i + 1) == idx)
+{
+new_node_ptr = malloc(sizeof(listint_t));
+if (!new_node_ptr)
+return (NULL);
+new_node_ptr->n = n;
+new_node_ptr->next = current_node->next;
+current_node->next = new_node_ptr;
+return (new_node_ptr);
+}
+return (NULL);
 }
