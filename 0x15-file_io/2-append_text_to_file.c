50f746d325267b1379cb77888191ab4c105e4084
EshietIy <eshietinyang@gmail.com>
2021-08-09 17:26:37 +0100

    a program that copies the content of a file to another file.

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,19 +8,20 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int d, status, i;
+int fd, len = 0, wstatus;
 if (filename == NULL)
 return (-1);
-if (text_content == NULL)
-return (1);
-d = open(filename, O_APPEND | O_WRONLY);
-if (f == -1)
+fd = open(filename, O_WRONLY | O_APPEND);
+if (fd == -1)
 return (-1);
-for (i = 0; text_content[i] != '\0'; i++)
-;
-status = write(d, text_content, i);
-if (status == -1)
+if (text_content)
+{
+while (text_content[len] != '\0')
+len++;
+wstatus = write(fd, text_content, len);
+if (wstatus == -1)
 return (-1);
-close(d);
+}
+close(fd);
 return (1);
 }
