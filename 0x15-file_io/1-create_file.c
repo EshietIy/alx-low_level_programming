0d5f08a35a428298cff8c8f770dfe0000c5b9ee3
EshietIy <eshietinyang@gmail.com>
2021-08-09 16:41:45 +0100

    a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *create_file - a function that create a file
+ *@filename: name of file
+ *@text_content: NULL terminated string
+ *Return: int 1 on success
+ */
+int create_file(const char *filename, char *text_content)
+{
+int file, i, w_count;
+if (!filename)
+return (-1)
+file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (create == -1)
+return (-1);
+if (text_content)
+{
+while (*(text_content + i))
+i++;
+w_count = write(file, text_content, i)
+if (w_count == -1)
+return (-1);
+}
+close(file);
+return (1);
+}
