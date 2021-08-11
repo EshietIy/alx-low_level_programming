6546c66b9ca49b9c1d4720d3b7588fdb7b75037d
EshietIy <eshietinyang@gmail.com>
2021-08-11 23:06:15 +0100

    elf file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,7 +7,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
+void ensure_endianness(char little_e, void *data, unsigned int len);
+void print_elf_header(void *h);
+void print_elf_header_2(void *h);
+void put_err(char *str);
 #endif
