fa6787bfe8f69a99dd5be6ac8d3cb1a021d0c2d8
EshietIy <eshietinyang@gmail.com>
2021-09-16 21:16:57 +0100

    a function that adds a new node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,11 +15,11 @@ if (new_node == NULL)
 free(new_node);
 return (NULL);
 }
-if (!head)
-return (new_node);
 new_node->n = n;
 new_node->next = *head;
 new_node->prev = NULL;
+if (*head)
+(*head)->prev = new_node;
 *head = new_node;
 return (new_node);
 }
