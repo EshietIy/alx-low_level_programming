eae84b4eaeb704ad056bf96d3df5c8c1f9edfdac
EshietIy <eshietinyang@gmail.com>
2021-09-16 22:44:35 +0100

    a function that inserts a new node at a given position.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -18,6 +18,8 @@ typedef struct dlistint_s
   struct dlistint_s *prev;
   struct dlistint_s *next;
 } dlistint_t;
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 void free_dlistint(dlistint_t *head);
