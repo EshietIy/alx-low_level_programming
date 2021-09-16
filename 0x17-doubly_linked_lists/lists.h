c063784e63d10c017791280e4724e3077e421bc3
EshietIy <eshietinyang@gmail.com>
2021-09-16 22:58:25 +0100

    a function that deletes the node at index index of a dlistint_t linked list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct dlistint_s
   struct dlistint_s *next;
 } dlistint_t;
 
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
