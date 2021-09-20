7f65ee919f71bdf006c82018da21bfb5232b29cc
EshietIy <eshietinyang@gmail.com>
2021-09-20 22:01:45 +0100

    shared lib

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -fpic -c *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=".:$LD_LIBRARY_PATH"
